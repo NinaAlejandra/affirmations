ccd58ff87778cc3345577e058b9e2b4d73b25dce
Nina Rivera <81374275+NinaAlejandra@users.noreply.github.com>
2022-09-26 03:03:29 +0200

    Add files via upload

---
+++ Affirmation generator/README.md
@@ -0,0 +1,11 @@
+Affirmation Generator
+Used quote code from https://www.divinectorweb.com/2022/03/random-quotes-generator-in-javascript.html?m=1 to build an affirmation generator
+
+Made this for a friends 30th birthday but can be used by whoever.
+
+The problem
+Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+I changed the original code a bit to match my intentions and I also made some minor changes to the styling. But most of it is borrowed code. Is consists of HTML, CSS and vanilla Javascript. I could have made an API instead but time was of the essence.
+
+View it live
+at:
---
+++ Affirmation generator/custom.js
@@ -0,0 +1,66 @@
+const affirmations = [
+  {
+    affirmation: `Jag är värdefull precis som jag är`,
+  },
+  {
+    affirmation: `På ett eller annat sätt så kommer allt att bli bra`,
+  },
+  {
+    affirmation: `Jag är inte ensam`,
+  },
+  {
+    affirmation: `– Jag är omgiven av kärlek`,
+  },
+  {
+    affirmation: `Jag är tillräcklig`,
+  },
+  {
+    affirmation: `Jag är trygg`,
+  },
+  {
+    affirmation: `Jag är stark`,
+  },
+  {
+    affirmation: `Jag förtjänar att vara lycklig`,
+  },
+  {
+    affirmation: `Jag är en fantastisk person`,
+  },
+  {
+    affirmation: `Jag är omtänksam`,
+  },
+  {
+    affirmation: `Jag får be om vad jag vill och vad jag behöver`,
+  },
+  {
+    affirmation: `Jag är komplett som jag är`,
+  },
+  {
+    affirmation: `ag lär mig fortfarande så det är okej att göra misstag`,
+  },
+  {
+    affirmation: `Jag hör hemma här, och jag förtjänar att ta plats`,
+  },
+  {
+    affirmation: `Jag kan vara mjuk i hjärtat och samtidigt stå fast vid mina gränser`,
+  },
+  {
+    affirmation: `Jag förtjänar stunder av tystnad`,
+  },
+  {
+    affirmation: `Jag har visdom bortom kunskap`,
+  },
+  {
+    affirmation: `Jag tar mig tid att uppleva sorg och ledsenhet när det behövs`,
+  },
+];
+
+let btn = document.querySelector("#Qbtn");
+
+let affirmation = document.querySelector(".affirmation");
+
+btn.addEventListener("click", function () {
+  let random = Math.floor(Math.random() * affirmations.length);
+
+  affirmation.innerHTML = affirmations[random].affirmation;
+});
